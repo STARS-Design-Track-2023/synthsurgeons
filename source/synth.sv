f8b892b5dcbc51674df19921eaa551fc3f7a75ef
bvincer <bvincer@purdue.edu>
2023-06-27 10:50:13 -0400

    updated code for top level and fixed n_rst

--- source/synth.sv
+++ source/synth.sv
@@ -16,31 +16,40 @@ module top
   input  logic txready, rxready
 );
 
+synth_top U1 (.keys(pb[12:0]), .modekey(pb[14]), .sound_edge(pb[15]), .en(pb[18]), .is_FPGA(pb[19]), .clk, .n_rst); 
+
 endmodule
 
-module synth_top (input logic [14:0] keypad_i, input logic clk, nrst, output logic pwm_o);
+module synth_top (input logic [14:0] keypad_i, input logic clk, n_rst, output logic pwm_o);
 
-/* insert keypad encoder port line*/
-
-logic [3:0] keycode, sound_series;
-logic mode_key, signal;
+keypad_encoder keypad_encoder (.keys(pb[12:0]), .modekey(pb[14]), .sound_edge(pb[15]), .clk, .n_rst);
+logic [12:0] keys;
+logic [3:0] out, keycode, sound_series;
+logic mode_edge,sound_edge;
 
 mode_fsm  mode_fsm (.clk, .n_rst);
 
 logic modekey_edge; 
 logic [1:0] mode, next_mode;
 
-frequency_divider freq_div (.keycode(pb[3:0]),.is_FPGA(pb[19]),.sound_series(pb[15:12]), .en(pb[18]), .divider({left,right}));
+frequency_divider freq_div (.is_FPGA(pb[19]),.en(pb[18]));
 
 logic [15:0] divider;
 
-/* insert sound series FSM port line*/
-/* insert variables related to sound series*/
-oscillator osc (.clk,.nrst, .en(pb[18]));
+sound_series_fsm sound_series_fsm (.n_rst, .clk);
+
+logic [3:0] note_out
+
+oscillator osc (.clk,.n_rst, .en(pb[18]));
 
 logic [15:0] count;
 
-sequential_div seq_div (.clk, .nrst);
+
+clock_div clock_div (.clk, .n_rst, .en(pb[18]));
+
+logic flag;
+
+sequential_div seq_div (.clk, .n_rst);
 
 logic [2:0] next_count,count_div;
 logic div, done, load;
@@ -50,11 +59,8 @@ logic [15:0] D, next_d, D_count, dividend;
 
 waveshaper waveshaper (/* idk what to put for  this port line, no clock or reset*/)
 
-logic [1:0] mode,
 logic [7:0] sample, quiotient;
 
 pwm pwm (.clk, .n_rst, /*pwm_o...is this variable supposed to be assigned to something on the FPGA board?*/)
-
-logic [7:0] sample;
 
 endmodule
